17f5f19f0e9d23ebad8c38e69540cda9f779866d
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-07-09 13:08:08 +0900

    feat/Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
