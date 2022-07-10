694f13e4a38cbad9e5f4e81a7d9a3ce0b8e103c3
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-07-10 18:41:35 +0900

    fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./css/Navbar.css";
 
-const Navbar = () => {
-  const [isMobile, setIsMobile] = useState(false);
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
-    <nav className="navbar">
-      <h3 className="logo">Logo</h3>
-      <ul
-        className={isMobile ? "nav-links-mobile" : "nav-links"}
-        onClick={() => setIsMobile(!isMobile)}
-      >
-        <Link to="/" className="home">
-          <li>Home</li>
-        </Link>
-        <Link to="/login" className="login1">
-          <li>로그인1</li>
-        </Link>
-        <Link to="/login" className="login1">
-          <li>로그인1</li>
-        </Link>
-        <Link to="/signup" className="signup">
-          <li>회원가입</li>
-        </Link>
-      </ul>
-      <button
-        className="mobile-meau-icon"
-        onClick={() => setIsMobile(!isMobile)}
-      >
-        {isMobile ? (
-          <i className="fas fa-times"></i>
-        ) : (
-          <i className="fas fa-bars"></i>
-        )}
-      </button>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Apple
+            <i className="fa-brands fa-apple" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/signup"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
   );
-};
+}
 
 export default Navbar;
